39269349e06be3bc49decaa5ef505e7c5b570363
Артём Гаранин <artem@garan.in>
2020-10-18 18:56:03 +0300

    ДЗ - Урок 2 - 03_favorite_movies

--- lesson_002/03_favorite_movies.py
+++ lesson_002/03_favorite_movies.py
@@ -15,4 +15,12 @@ my_favorite_movies = 'Терминатор, Пятый элемент, Ават�
 # Использовать .split() или .find()или другие методы строки нельзя - пользуйтесь только срезами,
 # как указано в задании!
 
-# TODO здесь ваш код
+film_1 = my_favorite_movies[:10]
+film_2 = my_favorite_movies[12:25]
+film_3 = my_favorite_movies[27:33]
+film_4 = my_favorite_movies[35:40]
+film_5 = my_favorite_movies[42:]
+
+film_list = [film_1, film_2, film_3, film_4, film_5]
+
+print(film_list[0] + '\n' + film_list[-1] + '\n' + film_list[1] + '\n' + film_list[-2])
