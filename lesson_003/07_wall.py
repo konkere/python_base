dd15b02bf85007afb7ff3328c34ec1e190f5eb6f
Артём Гаранин <artem@garan.in>
2020-10-25 22:42:15 +0300

    Lesson 003 - 07_wall - fix (чуть причесал, чтобы лишние кирпичи за границами экрана не рисовались)

--- lesson_003/07_wall.py
+++ lesson_003/07_wall.py
@@ -13,13 +13,16 @@ shift_y = 0
 
 for y in range(0, resolution_y, brick_height):
     shift_y += 1
-    for x in range(0, resolution_x + brick_width, brick_width):
-        if shift_y % 2 == 0:
-            shift_x = x - brick_width / 2
-        else:
-            shift_x = x
-        brick_point_left_bottom = simple_draw.get_point(shift_x, y)
-        brick_point_right_top = simple_draw.get_point(shift_x + brick_width, y + brick_height)
+    x = 0
+
+    if shift_y % 2 == 0:
+        shift_x = int(x - brick_width / 2)
+    else:
+        shift_x = x
+
+    for x in range(shift_x, resolution_x, brick_width):
+        brick_point_left_bottom = simple_draw.get_point(x, y)
+        brick_point_right_top = simple_draw.get_point(x + brick_width, y + brick_height)
         simple_draw.rectangle(brick_point_left_bottom, brick_point_right_top, simple_draw.COLOR_RED, 1)
 
 # Подсказки:
