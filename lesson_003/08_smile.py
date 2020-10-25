55712426a573baae271009e812b2cf374eb5d544
Артём Гаранин <artem@garan.in>
2020-10-25 23:47:36 +0300

    Lesson 003 - 08_smile

--- lesson_003/08_smile.py
+++ lesson_003/08_smile.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 # (определение функций)
+import random
 import simple_draw
 
 # Написать функцию отрисовки смайлика по заданным координатам
@@ -8,6 +9,28 @@ import simple_draw
 # Параметры функции: кордината X, координата Y, цвет.
 # Вывести 10 смайликов в произвольных точках экрана.
 
-# TODO здесь ваш код
+resolution_x, resolution_y = 1200, 800
+simple_draw.resolution = (resolution_x, resolution_y)
+
+
+def smile(x, y, color):
+    smile_center = simple_draw.get_point(x, y)
+    smile_eye_right = simple_draw.get_point(x - 15, y + 10)
+    smile_eye_left = simple_draw.get_point(x + 15, y + 10)
+    smile_nose_point1 = simple_draw.get_point(x, y + 5)
+    smile_nose_point2 = simple_draw.get_point(x - 5, y - 10)
+    smile_nose_point3 = simple_draw.get_point(x + 5, y - 10)
+    smile_nose = [smile_nose_point1, smile_nose_point2, smile_nose_point3]
+    smile_mouth_point1 = simple_draw.get_point(x - 20, y - 20)
+    smile_mouth_point2 = simple_draw.get_point(x + 20, y - 20)
+    simple_draw.circle(smile_center, 50, color, 3)
+    simple_draw.circle(smile_eye_left, 5, color, 3)
+    simple_draw.circle(smile_eye_right, 5, color, 3)
+    simple_draw.lines(smile_nose, color, width=3)
+    simple_draw.line(smile_mouth_point1, smile_mouth_point2, color, 3)
+
+
+for i in range(10):
+    smile(random.randint(50, resolution_x - 50), random.randint(50, resolution_y - 50), simple_draw.random_color())
 
 simple_draw.pause()
