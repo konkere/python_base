9b031e72f99a1238e33d14c9d31544954c5041a1
Артём Гаранин <artem@garan.in>
2020-10-25 22:29:12 +0300

    Lesson 003 - 07_wall

--- lesson_003/07_wall.py
+++ lesson_003/07_wall.py
@@ -6,7 +6,22 @@ import simple_draw
 # Нарисовать стену из кирпичей. Размер кирпича - 100х50
 # Использовать вложенные циклы for
 
-# TODO здесь ваш код
+resolution_x, resolution_y = 1200, 800
+simple_draw.resolution = (resolution_x, resolution_y)
+brick_width, brick_height = 100, 50
+shift_y = 0
+
+for y in range(0, resolution_y, brick_height):
+    shift_y += 1
+    for x in range(0, resolution_x + brick_width, brick_width):
+        if shift_y % 2 == 0:
+            shift_x = x - brick_width / 2
+        else:
+            shift_x = x
+        brick_point_left_bottom = simple_draw.get_point(shift_x, y)
+        brick_point_right_top = simple_draw.get_point(shift_x + brick_width, y + brick_height)
+        simple_draw.rectangle(brick_point_left_bottom, brick_point_right_top, simple_draw.COLOR_RED, 1)
+
 # Подсказки:
 #  Для отрисовки кирпича использовать функцию rectangle
 #  Алгоритм должен получиться приблизительно такой:
