7ed0e0ec6abe2e4009dc29e173a2bc46b2348409
Артём Гаранин <artem@garan.in>
2020-10-31 19:26:42 +0300

    Lesson 004 03_shape_select

--- lesson_004/03_shape_select.py
+++ lesson_004/03_shape_select.py
@@ -9,6 +9,76 @@ import simple_draw as sd
 # Код функций из упр lesson_004/02_global_color.py скопировать сюда
 # Результат решения см lesson_004/results/exercise_03_shape_select.jpg
 
-# TODO здесь ваш код
+resolution_x = 800
+resolution_y = 800
+sd.resolution = (resolution_x, resolution_y)
+sd.caption = 'Shape select'
+
+shapes_names = ('triangle', 'square', 'pentagon', 'hexagon')
+shapes = []
+color = sd.COLOR_YELLOW
+for i, j in enumerate(shapes_names):
+    print(i, '->', j)
+    shapes.append((i, j))
+
+
+user_input = input('Выберите фигуру (введите её номер): ')
+
+if user_input.isdecimal():
+    shape_number = int(user_input)
+    if 0 <= shape_number < 4:
+        shape_name = shapes[shape_number][1]
+        print('Вы выбрали:', shapes[shape_number][1])
+    else:
+        print('В списке нет такого номера. Нарисую треугольник.')
+        shape_name = 'triangle'
+elif user_input == '':
+    print('Вы не ввели номер фигуры. Нарисую треугольник.')
+    shape_name = 'triangle'
+else:
+    print('Вы ввели не номер. Нарисую треугольник.')
+    shape_name = 'triangle'
+
+
+def polygon_draw(polygon_point, start_angle, side_length, sides=3, color=color, line_width=3):
+    end_to_start_point = polygon_point
+    for side in range(sides - 1):
+        vector = sd.vector(start=end_to_start_point, angle=start_angle + 360*side/sides,
+                           length=side_length, width=line_width)
+        sd.line(end_to_start_point, vector, color, width=line_width)
+        end_to_start_point = vector
+    sd.line(end_to_start_point, polygon_point, color, width=line_width)
+
+
+def triangle_draw(triangle_point, start_angle, side_length, color, line_width=3):
+    sides = 3
+    polygon_draw(triangle_point, start_angle, side_length, sides, color, line_width=line_width)
+
+
+def square_draw(square_point, start_angle, side_length, color, line_width=3):
+    sides = 4
+    polygon_draw(square_point, start_angle, side_length, sides, color, line_width=line_width)
+
+
+def pentagon_draw(pentagon_point, start_angle, side_length, color, line_width=3):
+    sides = 5
+    polygon_draw(pentagon_point, start_angle, side_length, sides, color, line_width=line_width)
+
+
+def hexagon_draw(hexagon_point, start_angle, side_length, color, line_width=3):
+    sides = 6
+    polygon_draw(hexagon_point, start_angle, side_length, sides, color, line_width=line_width)
+
+
+shape_start_point = sd.get_point(resolution_x / 2 - 100, resolution_y / 2 - 100)
+
+if shape_name == 'triangle':
+    triangle_draw(shape_start_point, 0, 200, color)
+elif shape_name == 'square':
+    square_draw(shape_start_point, 0, 200, color)
+elif shape_name == 'pentagon':
+    pentagon_draw(shape_start_point, 0, 200, color)
+elif shape_name == 'hexagon':
+    hexagon_draw(shape_start_point, 0, 200, color)
 
 sd.pause()
