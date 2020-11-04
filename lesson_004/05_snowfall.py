54c4ec1eff0b2d5c286baab371d2ed3c79f4d4d7
user74 <housechel@mail.ru>
2020-11-04 23:32:36 +0500

    Lesson 004 05 - Corrected

--- lesson_004/05_snowfall.py
+++ lesson_004/05_snowfall.py
@@ -31,23 +31,19 @@ for _ in range(N):
     snowflakes.append([x, y, length])
 
 # -= Задание 1 =- ---------------------начало------------------------
-while True:
-    sd.clear_screen()
-    for snowflake_number in range(N):
-        x = snowflakes[snowflake_number][0]
-        y = snowflakes[snowflake_number][1]
-        length = snowflakes[snowflake_number][2]
-        sd.snowflake(center=sd.get_point(x, y), length=length)
-        if y > length:
-            # TODO списку по индексу снежинки присваиваем сразу 600 - верхняя граница чтобы она оттуда падала!
-            # Я вот эту тудушку не очень понял. Это же не про "нарисовать сверху новую снежинку, когда предыдущая упала"
-            # Тут просто отрисовывается падение: "если 'y' не близок к нулю, то уменьшать его"
-            # Это наверное было про 3-ю часть?
-            y = snowflakes.pop(snowflake_number)[1] - snowflake_speed
-            snowflakes.insert(snowflake_number, [x, y, length])
-    sd.sleep(0.1)
-    if sd.user_want_exit():
-        break
+# while True:
+#     sd.clear_screen()
+#     for snowflake_number in range(N):
+#         x = snowflakes[snowflake_number][0]
+#         y = snowflakes[snowflake_number][1]
+#         length = snowflakes[snowflake_number][2]
+#         sd.snowflake(center=sd.get_point(x, y), length=length)
+#         if y > length:
+#             y = snowflakes.pop(snowflake_number)[1] - snowflake_speed
+#             snowflakes.insert(snowflake_number, [x, y, length])
+#     sd.sleep(0.1)
+#     if sd.user_want_exit():
+#         break
 # -= Задание 1 =- ---------------------конец------------------------
 
 # Примерный алгоритм отрисовки снежинок
@@ -63,7 +59,6 @@ while True:
 #       прервать цикл
 
 
-# TODO Часть 2 (делается после зачета первой части)
 # Да ): Но я не удержался и сейчас её тоже пофиксил в соответсвии с текущим видом части 1
 #
 # Ускорить отрисовку снегопада
@@ -118,34 +113,36 @@ while True:
 # -= Задание 3 (усложненное) =- ---------------------начало------------------------
 # Тоже причесал
 
-# snowdrift = []
-#
-# while True:
-#     sd.start_drawing()
-#     for snowflake_number in range(N):
-#         x = snowflakes[snowflake_number][0]
-#         y = snowflakes[snowflake_number][1]
-#         length = snowflakes[snowflake_number][2]
-#         if y > length:
-#             sd.snowflake(center=sd.get_point(x, y), length=length, color=sd.background_color)
-#             x = snowflakes[snowflake_number][0] + sd.random_number(-20, 20)
-#             y = snowflakes.pop(snowflake_number)[1] - snowflake_speed
-#             snowflakes.insert(snowflake_number, [x, y, length])
-#         else:
-#             if snowflake_number in snowdrift:
-#                 pass
-#             else:
-#                 x = sd.random_number(0, resolution_x)
-#                 y = resolution_y
-#                 length = sd.random_number(10, 100)
-#                 snowflakes.append([x, y, length])
-#                 N += 1
-#                 snowdrift.append(snowflake_number)
-#         sd.snowflake(center=sd.get_point(x, y), length=length)
-#     sd.finish_drawing()
-#     sd.sleep(0.1)
-#     if sd.user_want_exit():
-#         break
+snowdrift = []
+
+while True:
+    sd.start_drawing()
+    for snowflake_number in range(N):
+        x = snowflakes[snowflake_number][0]
+        y = snowflakes[snowflake_number][1]
+        length = snowflakes[snowflake_number][2]
+        # TODO смущает большая вложенность данной части кода!
+        # TODO Опишите логику, построчно, будем дорабатывать!
+        if y > length:
+            sd.snowflake(center=sd.get_point(x, y), length=length, color=sd.background_color)
+            x = snowflakes[snowflake_number][0] + sd.random_number(-20, 20)
+            y = snowflakes.pop(snowflake_number)[1] - snowflake_speed
+            snowflakes.insert(snowflake_number, [x, y, length])
+        else:
+            if snowflake_number in snowdrift:
+                pass
+            else:
+                x = sd.random_number(0, resolution_x)
+                y = resolution_y
+                length = sd.random_number(10, 100)
+                snowflakes.append([x, y, length])
+                N += 1
+                snowdrift.append(snowflake_number)
+        sd.snowflake(center=sd.get_point(x, y), length=length)
+    sd.finish_drawing()
+    sd.sleep(0.1)
+    if sd.user_want_exit():
+        break
 # -= Задание 3 (усложненное) =- ---------------------конец------------------------
 
 sd.pause()
