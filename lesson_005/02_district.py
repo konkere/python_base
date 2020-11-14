07f676aa549bd2713bd27ce7c5b4cf98c2a2dcc5
Артём Гаранин <artem@garan.in>
2020-11-14 14:23:44 +0300

    Lesson 005 02 fix

--- lesson_005/02_district.py
+++ lesson_005/02_district.py
@@ -5,22 +5,18 @@
 # подсказка: для вывода элементов списка через запятую можно использовать функцию строки .join()
 # https://docs.python.org/3/library/stdtypes.html#str.join
 
-# TODO используем формат
-# TODO from имя_пакета.имя_модуля import имя_функции as короткое_название
-import district.central_street.house1.room1
-import district.central_street.house1.room2
-import district.central_street.house2.room1
-import district.central_street.house2.room2
-import district.soviet_street.house1.room1
-import district.soviet_street.house1.room2
-import district.soviet_street.house2.room1
-import district.soviet_street.house2.room2
+from district.central_street.house1.room1 import folks as folks_central_h1r1
+from district.central_street.house1.room2 import folks as folks_central_h1r2
+from district.central_street.house2.room1 import folks as folks_central_h2r1
+from district.central_street.house2.room2 import folks as folks_central_h2r2
+from district.soviet_street.house1.room1 import folks as folks_soviet_h1r1
+from district.soviet_street.house1.room2 import folks as folks_soviet_h1r2
+from district.soviet_street.house2.room1 import folks as folks_soviet_h2r1
+from district.soviet_street.house2.room2 import folks as folks_soviet_h2r2
 
-# TODO заранее создадим общий список
+folks_all = folks_central_h1r1 + folks_central_h1r2 + folks_central_h2r1 + folks_central_h2r2 +\
+            folks_soviet_h1r1 + folks_soviet_h1r2 + folks_soviet_h2r1 + folks_soviet_h2r2
 
-district_ppl = ', '.join(district.central_street.house1.room1.folks + district.central_street.house1.room2.folks +
-                         district.central_street.house2.room1.folks + district.central_street.house2.room2.folks +
-                         district.soviet_street.house1.room1.folks + district.soviet_street.house1.room2.folks +
-                         district.soviet_street.house2.room1.folks + district.soviet_street.house2.room2.folks)
+district_ppl = ', '.join(folks_all)
 
 print('На районе живут:', district_ppl)
