9f32b2a96f4ca8d6a615aacef64b27efe16af5ba
user74 <housechel@mail.ru>
2020-11-14 12:36:01 +0500

    Lesson 005 02 - Corrected

--- lesson_005/02_district.py
+++ lesson_005/02_district.py
@@ -5,6 +5,8 @@
 # подсказка: для вывода элементов списка через запятую можно использовать функцию строки .join()
 # https://docs.python.org/3/library/stdtypes.html#str.join
 
+# TODO используем формат
+# TODO from имя_пакета.имя_модуля import имя_функции as короткое_название
 import district.central_street.house1.room1
 import district.central_street.house1.room2
 import district.central_street.house2.room1
@@ -13,6 +15,8 @@ import district.soviet_street.house1.room1
 import district.soviet_street.house1.room2
 import district.soviet_street.house2.room1
 import district.soviet_street.house2.room2
+
+# TODO заранее создадим общий список
 
 district_ppl = ', '.join(district.central_street.house1.room1.folks + district.central_street.house1.room2.folks +
                          district.central_street.house2.room1.folks + district.central_street.house2.room2.folks +
