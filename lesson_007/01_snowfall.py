b6ece1099bf71943e1b0d28f7d195ce803c901c3
Артём Гаранин <artem@garan.in>
2020-11-24 20:41:42 +0300

    Lesson 007 01 (часть 1 и часть 2 в разных файлах)

--- lesson_007/01_snowfall.py
+++ lesson_007/01_snowfall.py
@@ -7,14 +7,37 @@ import simple_draw as sd
 #  - отработку изменений координат
 #  - отрисовку
 
+resolution_x = 1000
+resolution_y = 800
+sd.resolution = (resolution_x, resolution_y)
+speed = 10
+
 
 class Snowflake:
-    pass
 
-    # TODO здесь ваш код
+    def __init__(self, x, y, length):
+        self.x = x
+        self.y = y
+        self.length = length
+        self.color = sd.COLOR_WHITE
+
+    def clear_previous_picture(self):
+        snowlake_center = sd.get_point(self.x, self.y)
+        sd.snowflake(center=snowlake_center, length=self.length, color=sd.background_color)
+
+    def move(self):
+        self.y -= speed
+
+    def draw(self):
+        snowlake_center = sd.get_point(self.x, self.y)
+        sd.snowflake(center=snowlake_center, length=self.length, color=self.color)
+
+    def can_fall(self):
+        if self.y > 50:
+            return True
 
 
-flake = Snowflake()
+flake = Snowflake(int(resolution_x / 2), resolution_y - 100, 50)
 
 while True:
     flake.clear_previous_picture()
