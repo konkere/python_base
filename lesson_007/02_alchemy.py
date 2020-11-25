b03d3d1c22bebfa22442fae1c5fb5c160bb9830b
Артём Гаранин <artem@garan.in>
2020-11-25 22:59:53 +0300

    Lesson 007 02

--- lesson_007/02_alchemy.py
+++ lesson_007/02_alchemy.py
@@ -19,7 +19,120 @@
 #   print(Water(), '+', Air(), '=', Water() + Air())
 #   print(Fire(), '+', Air(), '=', Fire() + Air())
 
-# TODO здесь ваш код
+class Water:
+
+    def __str__(self):
+        return 'Вода'
+
+    def __add__(self, extra_element):
+        if extra_element is element_air:
+            return 'Шторм'
+        elif extra_element is element_fire:
+            return 'Пар'
+        elif extra_element is element_earth:
+            return 'Грязь'
+        elif extra_element is element_melange:
+            return 'Премеланжевая масса'
+
+
+class Air:
+
+    def __str__(self):
+        return 'Воздух'
+
+    def __add__(self, extra_element):
+        if extra_element is element_water:
+            return 'Шторм'
+        elif extra_element is element_fire:
+            return 'Молния'
+        elif extra_element is element_earth:
+            return 'Пыль'
+        elif extra_element is element_melange:
+            return 'Пряный газ'
+
+
+class Fire:
+
+    def __str__(self):
+        return 'Огонь'
+
+    def __add__(self, extra_element):
+        if extra_element is element_air:
+            return 'Молния'
+        elif extra_element is element_water:
+            return 'Пар'
+        elif extra_element is element_earth:
+            return 'Лава'
+        elif extra_element is element_melange:
+            return None
+
+
+class Earth:
+
+    def __str__(self):
+        return 'Земля'
+
+    def __add__(self, extra_element):
+        if extra_element is element_air:
+            return 'Пыль'
+        elif extra_element is element_fire:
+            return 'Лава'
+        elif extra_element is element_water:
+            return 'Грязь'
+        elif extra_element is element_melange:
+            return None
+
+
+#     \/  https://bit.ly/MelangeDune
+class Melange:
+
+    def __str__(self):
+        return 'Пряность'
+
+    def __add__(self, extra_element):
+        if extra_element is element_air:
+            return 'Пряный газ'
+        elif extra_element is element_fire:
+            return None
+        elif extra_element is element_earth:
+            return None
+        elif extra_element is element_water:
+            return 'Премеланжевая масса'
+
+
+def user_choice(welcome_txt):
+    while True:
+        choice_element = input('Введите номер {} элемента: '.format(welcome_txt))
+        if choice_element.isdigit() and 0 < int(choice_element) < 6:
+            verify_choice_element = int(choice_element) - 1
+            return verify_choice_element
+
+
+first_element = second_element = None
+
+element_water = Water()
+element_air = Air()
+element_fire = Fire()
+element_earth = Earth()
+element_melange = Melange()
+
+elements = [
+    element_water,
+    element_air,
+    element_fire,
+    element_earth,
+    element_melange
+]
+
+print('Выбери элемент из списка:')
+for number, element in enumerate(elements, start=1):
+    print('[{0}] → {1}'.format(number, element))
+
+first_element = user_choice('1-го')
+second_element = user_choice('2-го')
+
+print(elements[first_element], '+', elements[second_element], '=',
+      elements[first_element] + elements[second_element])
 
 # Усложненное задание (делать по желанию)
 # Добавить еще элемент в игру.
