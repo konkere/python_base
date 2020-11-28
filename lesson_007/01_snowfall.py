340ca106423ec7c035f715c7bfa35312a96b0da4
Артём Гаранин <artem@garan.in>
2020-11-28 14:30:46 +0300

    Lesson 007 01 fix (и вопрос в #комментариях)

--- lesson_007/01_snowfall.py
+++ lesson_007/01_snowfall.py
@@ -10,51 +10,59 @@ import simple_draw as sd
 resolution_x = 1000
 resolution_y = 800
 sd.resolution = (resolution_x, resolution_y)
-speed = 10
+flakes = []
 
 
 class Snowflake:
+
     # TODO предлагаю ничего не принимать на вход
-    def __init__(self, x, y, length):
+    def __init__(self):
         # TODO сделать код самодостаточным, определять рендомные значения в момент инициализации экземпляра класса
         # TODO не используя переменные из глобального скоупа
-        self.x = x
-        self.y = y
-        self.length = length
+        # Мне всё равно понадобятся значения размера экрана для генерации.
+        # Может их стоит передавать при создании объекта? Если нет, то получится вот так:
+        self.x = sd.randint(0, resolution_x)
+        self.y = sd.randint(resolution_y, resolution_y + 200)
+        self.length = sd.randint(10, 50)
         self.color = sd.COLOR_WHITE
+        self.speed = 10
 
     def clear_previous_picture(self):
         snowlake_center = sd.get_point(self.x, self.y)
         sd.snowflake(center=snowlake_center, length=self.length, color=sd.background_color)
 
     def move(self):
-        # TODO тут использовать метод, может_падать, если может то двигать икс и игрик
-        # TODO от куда у нас в классе появилось speed?
-        self.y -= speed
+        if self.can_fall():
+            self.x += sd.randint(-20, 20)
+            self.y -= self.speed
 
     def draw(self):
         snowlake_center = sd.get_point(self.x, self.y)
         sd.snowflake(center=snowlake_center, length=self.length, color=self.color)
 
     def can_fall(self):
-        # TODO напрашивается запись return self.y > 50
-        if self.y > 50:
-            return True
+        return self.y > 0
 
 
-flake = Snowflake(int(resolution_x / 2), resolution_y - 100, 50)
+def get_snowflakes(count):
+    global flakes
+    for _ in range(count):
+        flakes.append(Snowflake())
 
-while True:
-    flake.clear_previous_picture()
-    flake.move()
-    flake.draw()
-    if not flake.can_fall():
-        break
-    sd.sleep(0.1)
-    if sd.user_want_exit():
-        break
 
-# TODO вторую часть перенесите сюда уже доработанную по ТУДУ выше
+def get_fallen_flakes():
+    global flakes
+    count = 0
+    for flake in flakes:
+        if not flake.can_fall():
+            flake.clear_previous_picture()
+            flakes.remove(flake)
+            count += 1
+    return count
+
+
+# get_snowflakes(count=1)
+
 
 # шаг 2: создать снегопад - список объектов Снежинка в отдельном списке, обработку примерно так:
 # flakes = get_flakes(count=N)  # создать список снежинок
@@ -70,4 +78,19 @@ while True:
 #     if sd.user_want_exit():
 #         break
 
-sd.pause()
+
+get_snowflakes(count=50)
+
+while True:
+    sd.start_drawing()
+    for flake in flakes:
+        flake.clear_previous_picture()
+        flake.move()
+        flake.draw()
+    sd.finish_drawing()
+    fallen_flakes = get_fallen_flakes()
+    if fallen_flakes:
+        get_snowflakes(fallen_flakes)
+    sd.sleep(0.1)
+    if sd.user_want_exit():
+        break
