6ebdeec0dff8c868f23c2ff841c7ae0d4ec05d1e
user74 <housechel@mail.ru>
2020-12-15 00:22:21 +0500

    Lesson 008 01 - Completed

--- lesson_008/01_family.py
+++ lesson_008/01_family.py
@@ -474,3 +474,5 @@ cprint('За это время было заработано денег — {}, 
 #       for salary in range(50, 401, 50):
 #           max_cats = life.experiment(salary)
 #           print(f'При зарплате {salary} максимально можно прокормить {max_cats} котов')
+
+# зачет!
