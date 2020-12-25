0143285b618ea3b6538f7c15db66e697ecedde0b
Артём Гаранин <artem@garan.in>
2020-12-25 23:12:48 +0300

    Lesson 009 02 fix

--- lesson_009/02_log_parser.py
+++ lesson_009/02_log_parser.py
@@ -31,21 +31,23 @@ class EventsParser:
         self.file_out = file_out
         self.count = 0
         open(self.file_out, mode='w+').close()
+        self.parse_file()
 
     def parse_file(self):
         with open(self.file_in, 'r', encoding='utf8') as file:
             prev_date_time = ''
+            last_line = False
             for line in file:
-                prev_date_time = self.parse_line(line, prev_date_time)
+                prev_date_time = self.parse_line(line, prev_date_time, last_line)
+            last_line = True
+            self.parse_line(line, prev_date_time, last_line)
 
-    def parse_line(self, line, prev_date_time):
-        line_year = line[1:5]
-        line_month = line[6:8]
-        line_day = line[9:11]
-        line_hour = line[12:14]
-        line_minute = line[15:17]
-        line_date_time = f'[{line_year}.{line_month}.{line_day} {line_hour}:{line_minute}]'
-        if line_date_time != prev_date_time != '':
+    def parse_line(self, line, prev_date_time, last_line):
+        line_date_time = self.date_time(line)
+        if last_line:
+            line_out = f'{prev_date_time} {self.count}\n'
+            self.write_line(line_out)
+        elif line_date_time != prev_date_time != '':
             line_out = f'{prev_date_time} {self.count}\n'
             self.write_line(line_out)
             self.count = 0
@@ -53,18 +55,54 @@ class EventsParser:
             self.count += 1
         return line_date_time
 
+    def date_time(self, line):
+        line_year = line[1:5]
+        line_month = line[6:8]
+        line_day = line[9:11]
+        line_hour = line[12:14]
+        line_minute = line[15:17]
+        line_date_time = f'[{line_year}.{line_month}.{line_day} {line_hour}:{line_minute}]'
+        return line_date_time
+
     def write_line(self, line):
-        file_out = open(self.file_out, mode='a')
-        file_out.write(line)
-        file_out.close()
+        if self.count != 0:
+            file_out = open(self.file_out, mode='a')
+            file_out.write(line)
+            file_out.close()
 
 
-# TODO 0 нам не нужны в статистике
+class EventsParserByHour(EventsParser):
+
+    def date_time(self, line):
+        line_year = line[1:5]
+        line_month = line[6:8]
+        line_day = line[9:11]
+        line_hour = line[12:14]
+        line_date_time = f'[{line_year}.{line_month}.{line_day} {line_hour}h]'
+        return line_date_time
+
+
+class EventsParserByMonth(EventsParser):
+
+    def date_time(self, line):
+        line_year = line[1:5]
+        line_month = line[6:8]
+        line_date_time = f'[{line_year}.{line_month}]'
+        return line_date_time
+
+
+class EventsParserByYear(EventsParser):
+
+    def date_time(self, line):
+        line_year = line[1:5]
+        line_date_time = f'[{line_year}]'
+        return line_date_time
+
+
 event_parse = EventsParser('events.txt', 'events_nok_by_min.txt')
-event_parse.parse_file()
-
-# TODO этот вариант
-# Группировка -- это как с минутами? В этот час/месяц/год столько-то NOKов?
+event_parse_by_hour = EventsParserByHour('events.txt', 'events_nok_by_hour.txt')
+event_parse_by_month = EventsParserByMonth('events.txt', 'events_nok_by_month.txt')
+event_parse_by_year = EventsParserByYear('events.txt', 'events_nok_by_year.txt')
 
 # После зачета первого этапа нужно сделать группировку событий
 #  - по часам
