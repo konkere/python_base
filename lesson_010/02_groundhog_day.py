bf91420cdb9e241fa45269258126c8eab72a411f
Артём Гаранин <artem@garan.in>
2020-12-27 18:46:31 +0300

    Lesson 010 02

--- lesson_010/02_groundhog_day.py
+++ lesson_010/02_groundhog_day.py
@@ -17,8 +17,87 @@
 # При создании собственных исключений максимально использовать функциональность
 # базовых встроенных исключений.
 
-ENLIGHTENMENT_CARMA_LEVEL = 777
+from random import randint
 
-# TODO здесь ваш код
+
+class GroundhogError(Exception):
+
+    def __init__(self):
+        self.message = ''
+
+    def __str__(self):
+        return self.message
+
+
+class IamGodError(GroundhogError):
+
+    def __init__(self):
+        self.message = 'Азъ есмь Бог!'
+
+
+class DrunkError(GroundhogError):
+
+    def __init__(self):
+        self.message = 'Алкогольная интоксикация'
+
+
+class CarCrashError(GroundhogError):
+
+    def __init__(self):
+        self.message = 'Погиб в автокатастрофе'
+
+
+class GluttonyError(GroundhogError):
+
+    def __init__(self):
+        self.message = 'Обжорство сгубило'
+
+
+class DepressionError(GroundhogError):
+
+    def __init__(self):
+        self.message = 'Большое депрессивное расстройство'
+
+
+class SuicideError(GroundhogError):
+
+    def __init__(self):
+        self.message = 'Покончил с собой'
+
+
+def one_day():
+    day_carma = randint(1, 7)
+    day_error_dice = randint(0, 13)
+    if day_error_dice == 13:
+        raise error_rnd()
+    return day_carma
+
+
+def error_rnd():
+    errors = [
+        IamGodError(),
+        DrunkError(),
+        CarCrashError(),
+        GluttonyError(),
+        DepressionError(),
+        SuicideError(),
+    ]
+    return errors[randint(0, 5)]
+
+
+ENLIGHTENMENT_CARMA_LEVEL = 777
+day = 0
+carma = 0
+
+while carma < ENLIGHTENMENT_CARMA_LEVEL:
+    day += 1
+    print(f'-===== День {day} =====-')
+    try:
+        carma += one_day()
+    except GroundhogError as exc:
+        print(f'{exc}')
+    print(f'Карма: {carma}')
+
+print(f'Просветление произошло на {day} день')
 
 # https://goo.gl/JnsDqu
