9dbc7528164e533a5dbf9f7858b43184d2a22bde
Артём Гаранин <artem@garan.in>
2021-01-05 00:18:27 +0300

    Lesson 011 03

--- lesson_011/03_log_parser.py
+++ lesson_011/03_log_parser.py
@@ -14,4 +14,31 @@
 #
 # [2018-05-17 01:57] 1234
 
-# TODO здесь ваш код
+
+def grouped_event_generator(file_in):
+    with open(file_in, 'r', encoding='utf8') as file:
+        count = 0
+        current_date_time = ''
+        for line in file:
+            if len(line) == 33:
+                line_year = line[1:5]
+                line_month = line[6:8]
+                line_day = line[9:11]
+                line_hour = line[12:14]
+                line_minute = line[15:17]
+                line_date_time = f'{line_year}.{line_month}.{line_day} {line_hour}:{line_minute}'
+                if current_date_time == '':
+                    current_date_time = line_date_time
+                if line_date_time == current_date_time:
+                    count += 1
+                else:
+                    yield current_date_time, count
+                    count = 1
+                    current_date_time = line_date_time
+        if count > 0:
+            yield current_date_time, count
+
+
+grouped_events = grouped_event_generator('events.txt')
+for group_time, event_count in grouped_events:
+    print(f'[{group_time}] {event_count}')
