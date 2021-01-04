09e6e4266a8845e034faa0c38d9b4318cdbc0b37
Артём Гаранин <artem@garan.in>
2021-01-05 00:19:34 +0300

    Lesson 011 04

--- lesson_011/04_error_log_decorator.py
+++ lesson_011/04_error_log_decorator.py
@@ -6,20 +6,41 @@
 # Имя файла лога - function_errors.log
 # Формат лога: <имя функции> <параметры вызова> <тип ошибки> <текст ошибки>
 # Лог файл открывать каждый раз при ошибке в режиме 'a'
+import os.path
 
 
-def log_errors(func):
-    pass
-    # TODO здесь ваш код
+def log_errors(log_file):
+    open(log_file, mode='w').close()
+
+    def log_errors_sub(func):
+
+        def surrogate(*args, **kwargs):
+            try:
+                real_func = func(*args, **kwargs)
+            except Exception as exc:
+                params_call = []
+                for param_call in args:
+                    params_call.append(param_call)
+                for param_call in kwargs:
+                    params_call.append(f'{param_call}={kwargs[param_call]}')
+                line = f'{func.__name__} - {params_call} - {type(exc).__name__} - {exc}\n'
+                with open(log_file, mode='a') as file:
+                    file.write(line)
+            else:
+                return real_func
+
+        return surrogate
+
+    return log_errors_sub
 
 
 # Проверить работу на следующих функциях
-@log_errors
+@log_errors('function_errors.log')
 def perky(param):
     return param / 0
 
 
-@log_errors
+@log_errors('function_errors.log')
 def check_line(line):
     name, email, age = line.split(' ')
     if not name.isalpha():
@@ -38,13 +59,14 @@ lines = [
     'Земфира 86',
     'Равшан wmsuuzsxi@mail.ru 35',
 ]
+
 for line in lines:
     try:
         check_line(line)
     except Exception as exc:
         print(f'Invalid format: {exc}')
-perky(param=42)
 
+perky(param=42)
 
 # Усложненное задание (делать по желанию).
 # Написать декоратор с параметром - именем файла
