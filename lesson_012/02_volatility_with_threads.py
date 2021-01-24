885d320bae38130e4afd1e7f8c4c89b0e0c896b5
Артём Гаранин <artem@garan.in>
2021-01-24 23:04:45 +0300

    Lesson 012 02

--- lesson_012/02_volatility_with_threads.py
+++ lesson_012/02_volatility_with_threads.py
@@ -17,6 +17,61 @@
 #       ТИКЕР7, ТИКЕР8, ТИКЕР9, ТИКЕР10, ТИКЕР11, ТИКЕР12
 # Волатильности указывать в порядке убывания. Тикеры с нулевой волатильностью упорядочить по имени.
 #
-# TODO Внимание! это задание можно выполнять только после зачета lesson_012/01_volatility.py !!!
 
-# TODO тут ваш код в многопоточном стиле
+from threading import Thread
+from utils import time_track, volatility, result, files_in_dir
+
+
+class TickersParser(Thread):
+
+    def __init__(self, file, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.file = file
+        self.name_ticket = ''
+        self.volatility = 0
+        self.file_titles = 'SECID,TRADETIME,PRICE,QUANTITY\n'
+
+    def run(self):
+        price_min, price_max = self.parse_file()
+        self.volatility = volatility(price_min, price_max)
+
+    def parse_file(self):
+        prices = []
+        with open(self.file, 'r', encoding='utf8') as file:
+            for line in file:
+                if not line == self.file_titles:
+                    price = self.parse_line(line)
+                    prices.append(price)
+        prices.sort()
+        price_min = prices[0]
+        price_max = prices[-1]
+        return price_min, price_max
+
+    def parse_line(self, line):
+        if not self.name_ticket:
+            self.name_ticket = line.split(',')[0]
+        price = float(line.split(',')[2])
+        return price
+
+
+@time_track
+def main():
+    tickers = []
+    tickers_volatility = []
+    tickers_volatility_0 = []
+    for file in files_in_dir():
+        tickers.append(TickersParser(file))
+    for ticker in tickers:
+        ticker.start()
+    for ticker in tickers:
+        ticker.join()
+    for ticker in tickers:
+        if ticker.volatility == 0.0:
+            tickers_volatility_0.append(ticker.name_ticket)
+        else:
+            tickers_volatility.append([ticker.name_ticket, ticker.volatility])
+    result(tickers_volatility, tickers_volatility_0)
+
+
+if __name__ == '__main__':
+    main()
