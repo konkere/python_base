8e7dbbc03fd56c53bb43e78fef5f462ffeba566f
Артём Гаранин <artem@garan.in>
2021-01-25 22:55:41 +0300

    Lesson 012 03

--- lesson_012/03_volatility_with_processes.py
+++ lesson_012/03_volatility_with_processes.py
@@ -17,6 +17,68 @@
 #       ТИКЕР7, ТИКЕР8, ТИКЕР9, ТИКЕР10, ТИКЕР11, ТИКЕР12
 # Волатильности указывать в порядке убывания. Тикеры с нулевой волатильностью упорядочить по имени.
 #
-# TODO Внимание! это задание можно выполнять только после зачета lesson_012/02_volatility_with_threads.py !!!
 
-# TODO тут ваш код в многопроцессном стиле
+from multiprocessing import Process, Queue
+from utils import time_track, volatility, result, files_in_dir
+
+
+class TickersParser(Process):
+
+    def __init__(self, file, collector, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.file = file
+        self.collector = collector
+        self.name_ticker = ''
+        self.volatility = 0
+        self.file_titles = 'SECID,TRADETIME,PRICE,QUANTITY\n'
+
+    def run(self):
+        price_min, price_max = self.parse_file()
+        self.volatility = volatility(price_min, price_max)
+        self.collector.put(dict(ticker=self.name_ticker, volatility=self.volatility))
+
+    def parse_file(self):
+        prices = []
+        with open(self.file, 'r', encoding='utf8') as file:
+            for line in file:
+                if not line == self.file_titles:
+                    price = self.parse_line(line)
+                    prices.append(price)
+        prices.sort()
+        price_min = prices[0]
+        price_max = prices[-1]
+        return price_min, price_max
+
+    def parse_line(self, line):
+        if not self.name_ticker:
+            self.name_ticker = line.split(',')[0]
+        price = float(line.split(',')[2])
+        return price
+
+
+@time_track
+def main():
+    collector = Queue()
+    tickers = []
+    tickers_volatility = []
+    tickers_volatility_0 = []
+    for file in files_in_dir():
+        tickers.append(TickersParser(file, collector=collector))
+    for ticker in tickers:
+        ticker.start()
+    for ticker in tickers:
+        ticker.join()
+
+    while not collector.empty():
+        data = collector.get()
+        if data['volatility'] == 0.0:
+            tickers_volatility_0.append(data['ticker'])
+        else:
+            tickers_volatility.append([data['ticker'], data['volatility']])
+
+    result(tickers_volatility, tickers_volatility_0)
+
+
+# Intel 4 cores (8 threads) 3.6GHz base frequency (4.2GHz turbo) - Функция работала 0.5887 секунд(ы)
+if __name__ == '__main__':
+    main()
