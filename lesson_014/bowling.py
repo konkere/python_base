3d541370a7da03dd9d865c0e23fe555224ecd4d6
Артём Гаранин <artem@garan.in>
2021-02-14 19:58:31 +0300

    Lesson 014 fix with states add more tests

--- lesson_014/bowling.py
+++ lesson_014/bowling.py
@@ -1,5 +1,70 @@
 # -*- coding: utf-8 -*-
 
+from abc import ABCMeta, abstractmethod
+
+
+class Throw(metaclass=ABCMeta):
+
+    @abstractmethod
+    def digit(self, throws) -> int:
+        pass
+
+    @abstractmethod
+    def special(self) -> int:
+        pass
+
+
+class ThrowFirst(Throw):
+
+    def __init__(self):
+        self.throw = 0
+
+    def digit(self, throws) -> int:
+        return int(throws[self.throw])
+
+    def special(self) -> int:
+        return 20
+
+
+class ThrowSecond(Throw):
+
+    def __init__(self):
+        self.throw = 1
+
+    def digit(self, throws) -> int:
+        return int(throws[self.throw])
+
+    def special(self) -> int:
+        return 15
+
+
+class Frame:
+
+    def __init__(self, state, throws) -> None:
+        self.throws = throws
+        self._state = state
+        self.points = 0
+        self.special_symb = ['X', '/']
+        self.count()
+
+    def change_state(self, state) -> None:
+        self._state = state
+        self.count()
+
+    def count(self):
+        symbol = self.throws[self._state.throw]
+        sp_symbol = self.special_symb[self._state.throw]
+        if symbol == sp_symbol:
+            self.points = self._state.special()
+        elif symbol.isdigit():
+            number = self._state.digit(self.throws)
+            self.points += number
+            if self.points > 10 and number != 0:
+                raise SyntaxError('Ошибка формата фрейма (кеглей не может быть больше 10)')
+        else:
+            raise SyntaxError('Ошибка формата фрейма (неизвестные символы)')
+
+
 def score_by_frame(throws):
     result_names = {
         'strike': 20,
@@ -17,6 +82,8 @@ def score_by_frame(throws):
 
 
 def get_score(game_result):
+    if '0' in game_result:
+        raise SyntaxError('Ошибка формата результатов (недопустимый символ)')
     score = 0
     game_result_x2 = game_result.replace('X', 'X0').replace('-', '0')
     symbols = len(game_result_x2)
@@ -24,8 +91,13 @@ def get_score(game_result):
         raise SyntaxError('Ошибка количества бросков во фрейме')
     elif symbols > 20:
         raise SyntaxError('Ошибка количества фреймов (не более 10)')
+    throw_state_1 = ThrowFirst()
+    throw_state_2 = ThrowSecond()
     for frame_step in range(0, len(game_result_x2), 2):
         throw_1 = game_result_x2[frame_step]
         throw_2 = game_result_x2[frame_step + 1]
-        score += score_by_frame([throw_1, throw_2])
+        # score += score_by_frame([throw_1, throw_2])
+        frame = Frame(throw_state_1, [throw_1, throw_2])
+        frame.change_state(throw_state_2)
+        score += frame.points
     return score
--- lesson_014/tests/test_get_score.py
+++ lesson_014/tests/test_get_score.py
@@ -38,24 +38,82 @@ class GetScoreTest(unittest.TestCase):
         result = get_score('12X125-124/X12-88/')
         self.assertEqual(result, 95)
 
+    def test_more_than_10(self):
+        with self.assertRaises(SyntaxError):
+            get_score('99')
+
+    def test_multiple_more_than_10(self):
+        with self.assertRaises(SyntaxError):
+            get_score('9988448899778866')
+
+    def test_more_symbols(self):
+        with self.assertRaises(SyntaxError):
+            get_score('1111111111111111111111')
+
+    def test_rnd1(self):
+        result = get_score('X4/341412X513/1-X')
+        self.assertEqual(result, 112)
+
+    def test_rnd2(self):
+        result = get_score('X4/-41-12X5-3/1--9')
+        self.assertEqual(result, 93)
+
+    def test_99999999999999999999(self):
+        with self.assertRaises(SyntaxError):
+            get_score('99999999999999999999')
+
+    def test_rnd3(self):
+        with self.assertRaises(SyntaxError):
+            get_score('X4/-41-79X5-3/1--9')
+
+    def test_10_x_minus(self):
+        result = get_score('--------------------')
+        self.assertEqual(result, 0)
+
+    def test_10_x_minus_spare(self):
+        result = get_score('-/-/-/-/-/-/-/-/-/-/')
+        self.assertEqual(result, 150)
+
+    def test_10_x_strike(self):
+        result = get_score('XXXXXXXXXX')
+        self.assertEqual(result, 200)
+
+    def test_10_x_spare(self):
+        result = get_score('2/4/6/8/1/3/5/7/9/1/')
+        self.assertEqual(result, 150)
+
+    def test_spare_err(self):
+        with self.assertRaises(SyntaxError):
+            get_score('/2/4/6/8/1/3/5/7/9/1')
+
+    def test_unknown_symb(self):
+        with self.assertRaises(SyntaxError):
+            get_score('qwerasdfzxcvtyghbnui')
+
+    def test_rnd4(self):
+        with self.assertRaises(SyntaxError):
+            get_score('2/4/6/8/1/3/5/7/9/1/X')
+
+    def test_rnd5(self):
+        with self.assertRaises(SyntaxError):
+            get_score('2/4/6/8/1/3/5/7/9/1')
+
+    def test_empty(self):
+        result = get_score('')
+        self.assertEqual(result, 0)
+
+    def test_contain_zero(self):
+        with self.assertRaises(SyntaxError):
+            get_score('12014521212121212121')
+
+    def test_multiple_contains_zero(self):
+        with self.assertRaises(SyntaxError):
+            get_score('12014521210121012121')
+
+    def test_zero(self):
+        with self.assertRaises(SyntaxError):
+            get_score('0')
+
 
 if __name__ == '__main__':
     unittest.main()
-
-# TODO дополнить тесты на ошибки и не только
-
-# TODO дописать тесты на переполнение, на недополнение, на ноль! + такие тесты как к примеру:
-# TODO 'X4/341412X513/1-X'
-# TODO 'X4/-41-12X5-3/1--9'
-# TODO '99999999999999999999'
-# TODO 'X4/-41-79X5-3/1--9'
-# TODO '--------------------'
-# TODO '-/-/-/-/-/-/-/-/-/-/'
-# TODO 'XXXXXXXXXX'
-# TODO '2/4/6/8/1/3/5/7/9/1/'
-# TODO '/2/4/6/8/1/3/5/7/9/1'
-# TODO 'qwerasdfzxcvtyghbnui'
-# TODO '2/4/6/8/1/3/5/7/9/1/X'
-# TODO '2/4/6/8/1/3/5/7/9/1'
-# TODO ''
-# TODO чем больше тестов тем лучше
